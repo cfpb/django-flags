707312911dacf449e787a958a63cf24b35ab5ed9
Will Barton <william.barton@cfpb.gov>
2017-02-02 08:23:08 -0500

    Add flag_required decorator to top level import

--- flags/__init__.py
+++ flags/__init__.py
@@ -3,5 +3,6 @@ from flags.template_functions import (
     flags_enabled,
     flag_disabled
 )
+from flags.decorators import flag_required
 
-__all__ = ['flag_enabled', 'flags_enabled', 'flag_disabled']
+__all__ = ['flag_enabled', 'flags_enabled', 'flag_disabled', 'flag_required']
