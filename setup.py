5c93e987d433096bbe1fe789c5cecf3a43cd0a4a
Will Barton <william.barton@cfpb.gov>
2017-07-17 08:55:37 -0400

    Bump version for release

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     description='Feature flags for Wagtail sites',
     long_description=long_description,
     license='CC0',
-    version='2.0.4',
+    version='2.0.5',
     include_package_data=True,
     packages=find_packages(),
     install_requires=install_requires,
