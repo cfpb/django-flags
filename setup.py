174ef4e894912485069702d51e1125a41e57e54d
Will Barton <william.barton@cfpb.gov>
2018-08-29 16:04:24 -0400

    Bump to version 3.0.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     license='CC0',
-    version='3.0.1',
+    version='3.0.2',
     include_package_data=True,
     packages=find_packages(),
     install_requires=install_requires,
