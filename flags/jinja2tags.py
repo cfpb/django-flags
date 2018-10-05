d2878b5e5ead2c38a37c75009eb250dcad4618e1
Scott Cranfill <scott.cranfill@cfpb.gov>
2018-10-05 17:07:17 -0400

    Allow passing kwargs to flag_enabled and flag_disabled template tags
    Also adds an extension for Jinja2 tags, so that apps don't have to
    develop their own.

---
+++ flags/jinja2tags.py
@@ -0,0 +1,15 @@
+from jinja2 import contextfunction
+from jinja2.ext import Extension
+from templatetags.feature_flags import flag_disabled, flag_enabled
+
+
+class FlagsExtension(Extension):
+    def __init__(self, environment):
+        super(FlagsExtension, self).__init__(environment)
+        self.environment.globals.update({
+            'flag_enabled': contextfunction(flag_enabled),
+            'flag_disabled': contextfunction(flag_disabled),
+        })
+
+
+flags = FlagsExtension
