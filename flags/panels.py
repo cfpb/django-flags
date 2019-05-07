2e2268186a86e7ea79e62a2859db42bdec8624b8
Will Barton <william.barton@cfpb.gov>
2019-05-07 10:09:25 -0400

    Add a panel to list all flag checks in the current request cycle

--- flags/panels.py
+++ flags/panels.py
@@ -1,8 +1,20 @@
+import logging
+import traceback
+from collections import namedtuple
+
 from django.utils.translation import gettext_lazy as _
 
 from debug_toolbar.panels import Panel
+from flags import state
 from flags.middleware import FlagConditionsMiddleware
 from flags.sources import get_flags
+
+
+FlagCheck = namedtuple('FlagCheck', ['flag', 'traceback'])
+
+logger = logging.getLogger(__name__)
+
+_original_flag_state = state._flag_state
 
 
 class FlagsPanel(Panel):
@@ -25,5 +37,43 @@ class FlagsPanel(Panel):
             {
                 'request': request,
                 'flags': sorted(flags.values(), key=lambda x: x.name),
+            }
+        )
+
+
+class FlagChecksPanel(Panel):
+    """
+    A panel to display the current state and conditions of all feature flags
+    """
+
+    template = "flags/panels/flag_checks.html"
+    title = _("Flag Checks")
+
+    def __init__(self, *args, **kwargs):
+        super(FlagChecksPanel, self).__init__(*args, **kwargs)
+        self.checks = []
+
+    def enable_instrumentation(self):
+        # Monkey-patch flag checking to record where the call happens
+        def recording_flag_state(flag_name, **kwargs):
+            stack = traceback.extract_stack()
+            # Remove Django-Flags from the list
+
+            self.checks.append(
+                FlagCheck(flag_name, traceback.format_list(stack))
+            )
+            return _original_flag_state(flag_name, **kwargs)
+
+        state._flag_state = recording_flag_state
+
+    def disable_instrumentation(self):
+        # Restore the original functions
+        state._flag_state = _original_flag_state
+
+    def generate_stats(self, request, response):
+        self.record_stats(
+            {
+                'request': request,
+                'checks': self.checks,
             }
         )
--- flags/tests/test_panels.py
+++ flags/tests/test_panels.py
@@ -2,6 +2,7 @@ from django.test import RequestFactory, TestCase, override_settings
 
 from debug_toolbar.toolbar import DebugToolbar
 from flags.middleware import FlagConditionsMiddleware
+from flags.state import flag_state
 
 
 class FlagsPanelTestCase(TestCase):
@@ -28,3 +29,28 @@ class FlagsPanelTestCase(TestCase):
         self.panel.generate_stats(self.request, response)
         flags = self.panel.get_stats()['flags']
         self.assertIn('MYFLAG', [f.name for f in flags])
+
+
+class FlagChecksPanelTestCase(TestCase):
+
+    def setUp(self):
+        self.request = RequestFactory().get('/')
+        self.toolbar = DebugToolbar(self.request)
+        self.toolbar.stats = {}
+        self.panel = self.toolbar.get_panel_by_id('FlagChecksPanel')
+
+    @override_settings(FLAGS={'MYFLAG': [('boolean', True)]})
+    def test_recording(self):
+        self.assertEqual(len(self.panel.checks), 0)
+
+        self.panel.enable_instrumentation()
+        flag_state('MYFLAG')
+        self.panel.disable_instrumentation()
+
+        self.assertEqual(len(self.panel.checks), 1)
+
+        response = self.panel.process_request(self.request)
+        self.panel.generate_stats(self.request, response)
+        checks = self.panel.get_stats()['checks']
+
+        self.assertIn('MYFLAG', [c.flag for c in checks])
